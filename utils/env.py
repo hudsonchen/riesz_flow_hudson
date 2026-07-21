41bbd84547f2e84a09003bda1e78d07f17c200a5
hudsonchen <hudsonchen33@outlook.com>
2026-07-21 04:37:32 +0100

    u

--- utils/env.py
+++ utils/env.py
@@ -1,14 +1,28 @@
 from __future__ import annotations
 
+import os
+from pathlib import Path
+
+
+_PROJECT_ROOT = Path(__file__).resolve().parents[1]
+_PROJECT_CACHE = _PROJECT_ROOT / ".cache"
+_USER_CACHE = Path(os.environ.get("XDG_CACHE_HOME", Path.home() / ".cache")) / "w-flow"
+_DEFAULT_CACHE = _PROJECT_CACHE if os.access(_PROJECT_ROOT, os.W_OK) else _USER_CACHE
+_CACHE_ROOT = Path(os.environ.get("WFLOW_CACHE_ROOT", _DEFAULT_CACHE)).expanduser()
+
 HF_REPO_ID = "Goodeat/drifting"
-HF_ROOT = '/home/zongchen/mmd_flow_hudson/W-Flow/.cache/drifting_hf_root'
-VAE_HF_PATH = '/home/zongchen/mmd_flow_hudson/W-Flow/.cache/sdvae_hf_root'
-TORCH_HUB_DIR = "/home/zongchen/mmd_flow_hudson/W-Flow/.cache/torch_hub"
+HF_ROOT = os.environ.get("WFLOW_DRIFTING_HF_ROOT", str(_CACHE_ROOT / "drifting_hf_root"))
+VAE_HF_PATH = os.environ.get("WFLOW_VAE_HF_PATH", str(_CACHE_ROOT / "sdvae_hf_root"))
+TORCH_HUB_DIR = os.environ.get("TORCH_HUB_DIR", str(_CACHE_ROOT / "torch_hub"))
 
 WFLOW_HF_REPO_ID = "jiaqihan99/W-Flow"
-WFLOW_HF_ROOT = "/home/zongchen/mmd_flow_hudson/W-Flow/.cache/wflow_hf_root"
+WFLOW_HF_ROOT = os.environ.get("WFLOW_HF_ROOT", str(_CACHE_ROOT / "wflow_hf_root"))
 
-IMAGENET_PATH = "/home/zongchen/datasets/ILSVRC/Data/CLS-LOC"
-IMAGENET_CACHE_PATH = "/home/zongchen/mmd_flow_hudson/W-Flow/.cache/imagenet256-latents-sdvae"
-IMAGENET_FID_NPZ = "/home/zongchen/mmd_flow_hudson/W-Flow/.cache/wflow_hf_root/stats/jit_in256_stats.npz"
-IMAGENET_PR_NPZ = "/path/to/imagenet_val_prc_arr0.npz"  # or simply leave empty
+IMAGENET_PATH = os.environ.get("IMAGENET_PATH", str(Path.home() / "datasets/ILSVRC/Data/CLS-LOC"))
+IMAGENET_CACHE_PATH = os.environ.get(
+    "IMAGENET_CACHE_PATH", str(_CACHE_ROOT / "imagenet256-latents-sdvae")
+)
+IMAGENET_FID_NPZ = os.environ.get(
+    "IMAGENET_FID_NPZ", str(Path(WFLOW_HF_ROOT) / "stats/jit_in256_stats.npz")
+)
+IMAGENET_PR_NPZ = os.environ.get("IMAGENET_PR_NPZ", "")
